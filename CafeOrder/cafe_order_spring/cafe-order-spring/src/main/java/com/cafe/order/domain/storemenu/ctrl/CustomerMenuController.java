038675edb25fdb801040914383d6e7e2aa2efc4d
신은찬 <dmscks3126@gmail.com>
2025-12-10 17:02:38 +0900

    feat(#35): 구매자 전체 메뉴 조회 View 구현

--- CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/storemenu/ctrl/CustomerMenuController.java
+++ CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/storemenu/ctrl/CustomerMenuController.java
@@ -1,5 +1,6 @@
 package com.cafe.order.domain.storemenu.ctrl;
 
+import com.cafe.order.domain.menu.dto.Category;
 import com.cafe.order.domain.store.dto.Store;
 import com.cafe.order.domain.store.service.StoreService;
 import com.cafe.order.domain.storemenu.dto.CustomerMenuResponse;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequestMapping("/customer/menus")
 @Controller
@@ -23,16 +27,28 @@ public class CustomerMenuController {
         this.storeService = storeService;
     }
 
+    /**
+     * READ : 지점의 전체 메뉴 + 판매 가능 여부(재고 상태) 조회
+     */
     @GetMapping
     public String storeMenuList(Model model) {
         // TODO : 로그인 기능 이후 수정
         Integer storeId = 1; // 임시 강남점
         Store store = storeService.findById(storeId);
 
-        List<CustomerMenuResponse> menus = storeMenuService.findAllMenusWithAvailability(storeId);
+        if (store == null) {
+            throw new IllegalArgumentException("존재하지 않는 지점입니다: " + storeId);
+        }
+
+        List<CustomerMenuResponse> rawMenus = storeMenuService.findAllMenusWithAvailability(storeId);
+
+        // 카테고리 별 그룹핑
+        Map<Category, List<CustomerMenuResponse>> groupedMenus = rawMenus.stream()
+            .collect(Collectors.groupingBy(CustomerMenuResponse::getCategory));
 
         model.addAttribute("store", store);
-        model.addAttribute("menus", menus);
+        model.addAttribute("groupedMenus", groupedMenus);
+        model.addAttribute("allCategories", Categfory.values());
 
         return "customer/menus/list";
     }
