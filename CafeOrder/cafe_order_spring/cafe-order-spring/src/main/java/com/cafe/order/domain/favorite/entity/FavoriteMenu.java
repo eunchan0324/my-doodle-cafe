17de88fdcd4620ec6a4f299200cf52baec0d745a
신은찬 <dmscks3126@gmail.com>
2026-01-10 16:13:34 +0900

    refactor(#72): FavoriteMenu (Entity) 수ì  - 객체 참조

--- CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/favorite/entity/FavoriteMenu.java
+++ CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/favorite/entity/FavoriteMenu.java
@@ -1,10 +1,10 @@
 package com.cafe.order.domain.favorite.entity;
 
 import com.cafe.order.common.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.cafe.order.domain.menu.entity.Menu;
+import com.cafe.order.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "favorite_menu")
 @Entity
 public class FavoriteMenu extends BaseEntity {
@@ -20,17 +20,25 @@ public class FavoriteMenu extends BaseEntity {
     @EmbeddedId
     private FavoriteMenuId id;
 
+    @MapsId("userId") // FavoriteMenuId.userId와 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @MapsId("menuId") // FavoriteMenuId.menuId와 매핑됨
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    public FavoriteMenu(String cusotmerId, UUID menuId) {
-        this.id = new FavoriteMenuId(cusotmerId, menuId);
+    // 생성자
+    public FavoriteMenu(User user, Menu menu) {
+        this.user = user;
+        this.menu = menu;
+        // PK 객체도 생성해서 넣어줘야 영속성 컨텍스트가 인식함
+        this.id = new FavoriteMenuId(user.getId(), menu.getId());
         this.createdAt = LocalDateTime.now();
-    }
-
-    // DB 조회용 생성자
-    public FavoriteMenu(FavoriteMenuId id, LocalDateTime createdAt) {
-        this.id = id;
-        this.createdAt = createdAt;
     }
 }
