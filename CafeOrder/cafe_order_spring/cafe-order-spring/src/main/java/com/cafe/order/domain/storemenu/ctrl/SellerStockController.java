8a974551c145e82946c704f455bc47a2b64283e1
신은찬 <dmscks3126@gmail.com>
2026-01-10 14:27:54 +0900

    refactor(#74): menustatus 패키지 삭제 및 SellerStockController storeMenu 패키지로 이동 ë°

--- CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/storemenu/ctrl/SellerStockController.java
+++ CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/storemenu/ctrl/SellerStockController.java
@@ -1,9 +1,11 @@
-package com.cafe.order.domain.menustatus.ctrl;
+package com.cafe.order.domain.storemenu.ctrl;
 
 import com.cafe.order.domain.storemenu.entity.SalesStatus;
-import com.cafe.order.domain.menustatus.service.SellerStockService;
 import com.cafe.order.domain.store.entity.Store;
 import com.cafe.order.domain.store.service.StoreService;
+import com.cafe.order.domain.storemenu.entity.StoreMenu;
+import com.cafe.order.domain.storemenu.service.StoreMenuService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +15,11 @@ import java.util.UUID;
 
 @RequestMapping("/seller/stock")
 @Controller
+@RequiredArgsConstructor
 public class SellerStockController {
 
-    private final SellerStockService sellerStockService;
+    private final StoreMenuService storeMenuService;
     private final StoreService storeService;
-
-    public SellerStockController(SellerStockService sellerStockService, StoreService storeService) {
-        this.sellerStockService = sellerStockService;
-        this.storeService = storeService;
-    }
 
     /**
      * READ : 재고/판매 상태 조회
@@ -32,13 +30,12 @@ public class SellerStockController {
         Integer storeId = 1; // 임시 강남점 (1)
         Store store = storeService.findById(storeId);
 
-        List<MenuStatus> menuStatusList = sellerStockService.findByStoreId(storeId);
+        List<StoreMenu> storeMenus = storeMenuService.findByStoreId(storeId);
 
         model.addAttribute("store", store);
-        model.addAttribute("menuStatuses", menuStatusList);
+        model.addAttribute("menuStatuses", storeMenus);
 
         return "seller/menustatus/manage";
-
     }
 
     /**
@@ -46,11 +43,11 @@ public class SellerStockController {
      */
     @PostMapping("/{menuId}/update")
     public String updateMenuStatus(@PathVariable UUID menuId,
-                              @RequestParam int stock, @RequestParam SalesStatus status) {
+                                   @RequestParam int stock,
+                                   @RequestParam SalesStatus status) {
         // TODO : 실제로는 로그인한 판매자의 storeId 가져오기
         Integer storeId = 1; // 임시 강남점 (1)
-        sellerStockService.updateMenuStatus(storeId, menuId, stock, status);
+        storeMenuService.updateStockAndStatus(storeId, menuId, stock, status);
         return "redirect:/seller/stock";
     }
-
 }
