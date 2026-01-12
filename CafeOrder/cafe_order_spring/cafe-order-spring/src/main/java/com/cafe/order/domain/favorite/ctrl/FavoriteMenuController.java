b30213d3a5e3d000e01fb5144154899e661e2e95
신은찬 <dmscks3126@gmail.com>
2026-01-12 20:14:43 +0900

    refactor(#57): favorite 도메인 로그인 기능 완성에 따른 todo 처리

--- CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/favorite/ctrl/FavoriteMenuController.java
+++ CafeOrder/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/domain/favorite/ctrl/FavoriteMenuController.java
@@ -1,9 +1,10 @@
 package com.cafe.order.domain.favorite.ctrl;
 
 import com.cafe.order.domain.favorite.dto.FavoriteMenuResponse;
-import com.cafe.order.domain.favorite.entity.FavoriteMenu;
 import com.cafe.order.domain.favorite.service.FavoriteMenuService;
+import com.cafe.order.global.security.dto.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +23,13 @@ public class FavoriteMenuController {
      * READ : 구매자 찜 목록 조회
      */
     @GetMapping
-    public String favoriteMenuList(Model model) {
-        // todo : 로그인 후 수정
-        Integer userId = 1;
+    public String favoriteMenuList(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+        // 로그인 방어 로직
+        if (userDetails == null) {
+            return "redirect:/login";
+        }
+
+        Integer userId = userDetails.getId();
 
         List<FavoriteMenuResponse> favorites = favoriteMenuService.favoriteMenuList(userId);
 
@@ -37,10 +42,17 @@ public class FavoriteMenuController {
      * POST : 찜 상태 변경
      */
     @PostMapping("/{menuId}/toggle")
-    public String toggleFavorite(@PathVariable UUID menuId,
-                                 @RequestParam(defaultValue = "") String redirect) {
-        // todo : 로그인 이후 수정
-        Integer userId = 1;
+    public String toggleFavorite(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable UUID menuId,
+            @RequestParam(defaultValue = "") String redirect) {
+
+        // 로그인 방어 로직
+        if (userDetails == null) {
+            return "redirect:/login";
+        }
+
+        Integer userId = userDetails.getId();
 
         favoriteMenuService.toggleFavorite(userId, menuId);
 
